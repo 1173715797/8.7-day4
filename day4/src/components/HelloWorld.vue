eb1f5b16a52f0ed2d5a9981654ad5fd2af3cd284
changjinyu <1173715797@qq.com>
2019-08-07 08:56:29 +0800

    one

---
+++ day4/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  props:{
+
+  },
+  components:{
+
+  },
+  data(){
+    return {
+      Arr=[a,2,4,'5g',h]
+
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+    Arr.forEach((item)=>{
+    Consolo.log(item)//a,2,4,5g,h
+  })
+  },
+  created(){
+
+  },
+  mounted(){
+
+  }
+}
+</script>
+<style scoped lang="">
+
+</style>
